c98ae74baf993b8f990eda095330142397e00bdf
ThiagoMontezano <thiagomontezano@yahoo.com.br>
2017-06-03 14:26:12 -0300

    alterando configuracoes e incluindo entidade seessao

---
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -0,0 +1,50 @@
+package br.com.caelum.ingresso.model;
+
+import java.time.LocalTime;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Sessao {
+	
+    @Id
+    @GeneratedValue
+    private Integer id;	
+	private Filme filme;
+	private Sala sala;
+	private LocalTime horario;
+		
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+    public Filme getFilme() {
+        return filme;
+    }
+
+    public void setFilme(Filme filme) {
+        this.filme = filme;
+    }
+    
+    public Sala getSala() {
+        return sala;
+    }
+
+    public void setSala(Sala sala) {
+        this.sala = sala;
+    }
+
+	public LocalTime getHorario() {
+		return horario;
+	}
+
+	public void setHorario(LocalTime horario) {
+		this.horario = horario;
+	}    
+
+}
