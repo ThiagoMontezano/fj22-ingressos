7eb191f33f549b1eeefe4b31c79b2028f38e5b2e
ThiagoMontezano <thiagomontezano@yahoo.com.br>
2017-06-03 16:52:32 -0300

    Iniciando testes unitarios

---
+++ src/test/java/br/com/caelum/ingresso/validation/GerenciadorDeSessoesTest.java
@@ -0,0 +1,14 @@
+package br.com.caelum.ingresso.validation;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.junit.Test;
+
+public class GerenciadorDeSessoesTest {
+
+	@Test
+	public void deveAdicionarSeListaDaSessaoEstiverVazia(){
+		//List<Sessao> sessoes = Collections.emptyList();
+	}
+}
