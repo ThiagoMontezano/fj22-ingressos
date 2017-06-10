42ea13703822dca3c8bec3578b00344b5e2d2bd6
ThiagoMontezano <thiagomontezano@yahoo.com.br>
2017-06-10 15:12:29 -0300

    selecionando lugares

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -4,19 +4,24 @@ import br.com.caelum.ingresso.dao.FilmeDao;
 import br.com.caelum.ingresso.dao.SalaDao;
 import br.com.caelum.ingresso.dao.SessaoDao;
 import br.com.caelum.ingresso.model.GerenciadorDeSessoes;
+import br.com.caelum.ingresso.model.ImagemCapa;
 import br.com.caelum.ingresso.model.Sessao;
 import br.com.caelum.ingresso.model.form.SessaoForm;
+import br.com.caelum.ingresso.rest.ImdbClient;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class SessaoController {
@@ -29,6 +34,9 @@ public class SessaoController {
 	
 	@Autowired
 	private SessaoDao sessaoDao;
+	
+    @Autowired
+    private ImdbClient client;
 	
 	@GetMapping("/sessao")
 	public ModelAndView form(@RequestParam("salaId") Integer salaId, SessaoForm form){
@@ -56,6 +64,18 @@ public class SessaoController {
 			return new ModelAndView("redirect:/sala/" + form.getSalaId() + "/sessoes");
 		}
 		return form(form.getSalaId(), form);
+	}
+	
+	@GetMapping("/sessao/{id}/lugares")
+	public ModelAndView lugaresNaSessao(@PathVariable("id") Integer sessaoId){
+		ModelAndView modelAndView = new ModelAndView("sessao/lugares");
+		
+		Sessao sessao = sessaoDao.findOne(sessaoId);
+		Optional<ImagemCapa> imagemCapa = client.request(sessao.getFilme(), ImagemCapa.class);
+		
+		modelAndView.addObject("sessao",sessao);
+		modelAndView.addObject("imagemCapa",imagemCapa.orElse(new ImagemCapa()));
+		return modelAndView;
 	}
 	
 }
--- src/main/java/br/com/caelum/ingresso/rest/ImdbClient.java
+++ src/main/java/br/com/caelum/ingresso/rest/ImdbClient.java
@@ -3,24 +3,25 @@ package br.com.caelum.ingresso.rest;
 import java.net.URI;
 import java.util.Optional;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
-
-import br.com.caelum.ingresso.model.DetalhesDoFilme;
 import br.com.caelum.ingresso.model.Filme;
 
 @Component
 public class ImdbClient {
 
-	public Optional<DetalhesDoFilme> request(Filme filme) {
+	private Logger logger = Logger.getLogger(ImdbClient.class);
+	
+	public <T> Optional<T> request(Filme filme, Class<T> classe) {
 		RestTemplate client	= new RestTemplate();
 		String url = "https://imdb-fj22.herokuapp.com/imdb?title=%s";
 		URI uri = URI.create(String.format(url, filme.getNome().replace(" ", "+")));
 		try	{
-			DetalhesDoFilme	detalhesDoFilme	= client.getForObject(uri, DetalhesDoFilme.class);
-			return Optional.of(detalhesDoFilme);
+			return Optional.of(client.getForObject(uri, classe));
 		} catch (RestClientException e)	{
+			logger.error(e.getMessage(),e);
 			return	Optional.empty();
 		}
 	}
